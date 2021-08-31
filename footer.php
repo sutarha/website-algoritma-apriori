d892a3c0f41141f8906c6324e9256bb53fee100b
sutarha <44222371+sutarha@users.noreply.github.com>
2021-08-31 20:12:39 +0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                MBA Apriori. Copyright &copy; <a href="">skripsi<a/>
+            </span>
+        </div>
+    </div>
+</div>
---
+++ home.php
@@ -0,0 +1,33 @@
+<div class="main-content">
+    <div class="main-content-inner">
+        <div class="page-content">
+            <div class="page-header">
+                <h1>
+                    Beranda
+                </h1>
+            </div><!-- /.page-header -->
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- PAGE CONTENT BEGINS -->
+
+
+                    <div class="row">
+                        <center>
+                            <h1>
+                                Implementasi market basket analisis Data Mining Rule Asosiasi Menggunakan Algoritma Apriori<br>dalam Penentuan Pola Pembelian Transaksi
+                            </h1>
+                            <div style="text-align:center;"><img width="320px" hight="auto" src=""></div>
+                            <h2>
+                                (Studi Kasus: toko clothes)
+                            </h2>
+                        </center>
+
+                    </div><!-- /.row -->
+
+                    <!-- PAGE CONTENT ENDS -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.page-content -->
+    </div>
+</div><!-- /.main-content -->
